2f926d727f8d9a414b0b5024aaed3c04b353c3c6
J C Lawrence <claw@kanga.nu>
2015-07-26 15:29:27 -0700

    auto-pyver

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 #! /usr/bin/env python
 
+try:
+  import pyver # pylint: disable=W0611
+except ImportError:
+  import os, subprocess
+  try:
+    environment = os.environ.copy()
+    cmd = "pip install pyver".split (" ")
+    subprocess.check_call (cmd, env = environment)
+  except subprocess.CalledProcessError:
+    import sys
+    print >> sys.stderr, "Problem installing 'pyver' dependency."
+    print >> sys.stderr, "Please install pyver manually."
+    sys.exit (1)
+  import pyver # pylint: disable=W0611
+
 from setuptools import setup, find_packages
 
-__version__ = "unknown"
+__version__, __version_info__ = pyver.get_version (pkg = "configtool")
 
 exec open ("configtool/version.py")
 
@@ -12,10 +27,10 @@ setup (
   description = "Configtool configuration management suite",
   long_description = "Confgtool configuration mangement suite",
   classifiers = [],
-  keywords = "",
+  keywords = "configuration management",
   author = "J C Lawrence",
   author_email = "claw@kanga.nu",
-  url = "http://kanga.nu/~claw/",
+  url = "https://github.com/clearclaw/configtool",
   license = "Creative Commons Attribution-ShareAlike 3.0 Unported",
   packages = find_packages (exclude = ["tests",]),
   package_data = {
